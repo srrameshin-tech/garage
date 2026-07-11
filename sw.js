b9744b771a9d0e08087560c75425e66c7df3772e
srrameshin-tech <srrameshin@gmail.com>
2026-07-11 21:06:59 +0530

    Rebrand to GarageBook

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ridekhata-v1';
+const CACHE_NAME = 'garagebook-v1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
